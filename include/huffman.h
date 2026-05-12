a5327b7e1a5a90daa0ff1faa7c27a2a164ec0d73
Ryan Ferrer <ryanferrer158@gmail.com>
2026-05-12 19:51:05 -0400

    Added file header

--- include/huffman.h
+++ include/huffman.h
@@ -1,5 +1,22 @@
+/**
+ * @file huffman.h
+ * @brief Public interface for Huffman coding compression and decompression.
+ *
+ * This module provides the core functionality for building Huffman trees,
+ * generation variable-length codes, and performing losless compression and
+ * decompression of files.
+ *
+ * The implementation operates on raw bytes, allowing it to compress both text
+ * and arbitrary binary files.
+ *
+ * @author Ryan Ferrer
+ * @date 2026-05-12
+ */
 
-#ifndef FILE_COMPRESSION_HUFFMAN_H
-#define FILE_COMPRESSION_HUFFMAN_H
 
-#endif //FILE_COMPRESSION_HUFFMAN_H
+#ifndef HUFFMAN_H
+#define HUFFMAN_H
+
+
+
+#endif // HUFFMAN_H
