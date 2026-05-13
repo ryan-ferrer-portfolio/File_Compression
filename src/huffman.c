7d26ed621c311f1e24fad9522c24863bb080fe50
Ryan Ferrer <ryanferrer158@gmail.com>
2026-05-12 21:49:49 -0400

    Added file header and function definitions

--- src/huffman.c
+++ src/huffman.c
@@ -1,3 +1,23 @@
-//
-// Created by ryanf on 5/12/2026.
-//
+/**
+ * @file huffman_compress.c
+ * @brief High-level compression and decompression entry points for Huffman coding.
+ *
+ * This module implements the top-level Huffman compression and decompression
+ * pipelines. Each public function coordinates the major stages of the algorithm
+ * while delegating detailed work to helper modules.
+ *
+ * @author Ryan Ferrer
+ * @date 2026-05-12
+ */
+
+#include "../include/huffman.h"
+
+#include <stdio.h>
+
+int huffman_compress_file(FILE* in_fp, FILE* out_fp) {
+
+}
+
+int huffman_decompress_file(FILE* in_fp, FILE* out_fp) {
+
+}
