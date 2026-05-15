992c41ebd3e07c861d657afb8f4be4a087a46a37
Ryan Ferrer <ryanferrer158@gmail.com>
2026-05-15 14:50:26 -0400

    Implemented build_frequency_table

--- src/huffman_compress.c
+++ src/huffman_compress.c
@@ -3,3 +3,11 @@
 //
 
 #include "../include/huffman_compress.h"
+
+void build_frequency_table(FILE* in_fp, uint64_t freq[NUM_BYTE_VALUES]) {
+    int byte;
+
+    while((byte = fgetc(in_fp)) != EOF) {
+        freq[(unsigned char) byte]++;
+    }
+}
